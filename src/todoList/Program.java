c0994b42a5f4acdb14a6addd554614cc9ea90f48
Leonardo Vinicius <leonardoviniciuswd@gmail.com>
2022-08-09 19:06:18 -0300

    .

--- src/todoList/Program.java
+++ src/todoList/Program.java
@@ -12,29 +12,68 @@ public class Program {
 		List<String> todoList = new ArrayList<>();
 		String task;
 		char response;
+		int taskPosition = 0;
+		boolean stopProgram = false;
 		
-		System.out.println("====================================================================");
-		System.out.println("                        To Do List          ");
-		System.out.println("====================================================================");
-		System.out.print("(0)add (1)remove (2)delete (3)Done,"
-					+ "(4)show the list or (5)exit: ");
-		int actionNumber = sc.nextInt();
-		switch(actionNumber) {
-			case 0:
-				do {
+		do {
+			System.out.println("====================================================================");
+			System.out.println("                        To Do List          ");
+			System.out.println("====================================================================");
+			System.out.print("(0)add (1)remove (2)Task completed,"
+						+ "(3)show the list, (4)exit: ");
+			int actionNumber = sc.nextInt();
+			switch(actionNumber) {
+				case 0:
+					do {
+						System.out.println();
+						System.out.print("New task: ");
+						sc.nextLine();
+						task = sc.nextLine();
+						todoList.add(task);
+						System.out.print("Add a new task? (s/n)");
+						response = sc.next().charAt(0);
+					} while(response == 's' || response == 'S');
 					System.out.println();
-					System.out.print("New task: ");
-					sc.nextLine();
-					task = sc.nextLine();
-					todoList.add(task);
-					System.out.println("Add a new task? (s/n)");
+				break;
+				case 1:
+					System.out.println();
+					System.out.print("Type the number of the task to be removed: ");
+					actionNumber = sc.nextInt();
+					String removeTask = todoList.remove(actionNumber);
+					System.out.println(removeTask + " removed");
+					System.out.println("Back to main menu? (s/n)");
 					response = sc.next().charAt(0);
-				} while(response == 's' || response == 'S');
+						if (response == 'n' || response == 'N') {
+							stopProgram = true;
+						}
+					break;
+				case 2:
+					System.out.print("Number of the task completed: ");
+					actionNumber = sc.nextInt();
+					break;
+				case 3:
+					System.out.println();
+					for (String list : todoList) {
+						System.out.println(taskPosition +": " + list);
+						taskPosition += 1;
+					}
+					System.out.println("Back to main menu? (s/n)");
+					response = sc.next().charAt(0);
+						if (response == 'n' || response == 'N') {
+							stopProgram = true;
+						}
+					break;
+				case 4:
+					stopProgram = true;
+				}
+			
+			for(int i = 0; i < 10; i++) {
+				System.out.println();
 			}
+	        
+		} while(!stopProgram);
 		
-		for (String list : todoList) {
-			System.out.println("> " + list);
-		}
+		System.out.println();
 		
 		sc.close();
 		}		
